0441654a717167916df98fc5c422e006df284668
Dimarata <ivanov_dimo@abv.bg>
2020-02-28 16:29:28 +0200

    Add UserTask navigation property to User

--- TaskManagement/TaskManagement.Entities/User.cs
+++ TaskManagement/TaskManagement.Entities/User.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 
 namespace TaskManagement.Entities
 {
    public class User : IdentityUser
     {
-
+        public ICollection<UserTask> UserTasks { get; set; }
     }
 }
