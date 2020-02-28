d50581ed730014c5146722394c8c7cef045abfde
Dimarata <ivanov_dimo@abv.bg>
2020-02-28 16:29:05 +0200

    Added Task

--- TaskManagement/TaskManagement.Entities/Task.cs
+++ TaskManagement/TaskManagement.Entities/Task.cs
@@ -6,6 +6,10 @@ namespace TaskManagement.Entities
 {
     public class Task : BaseEntity
     {
-
+        public DateTime DueDate { get; set; }
+        public string Description { get; set; }
+        public ICollection<UserTask> UserTasks { get; set; }
+        public DateTime NextActionDate { get; set; }
+        public ICollection<Comment> Comments { get; set; }
     }
 }
