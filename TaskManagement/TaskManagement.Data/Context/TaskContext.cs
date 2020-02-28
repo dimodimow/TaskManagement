15b2c924b419eb1e95457926a9714e7d52d47331
Dimarata <ivanov_dimo@abv.bg>
2020-02-28 16:27:51 +0200

    Configured ManyToMany releationship.

--- TaskManagement/TaskManagement.Data/Context/TaskContext.cs
+++ TaskManagement/TaskManagement.Data/Context/TaskContext.cs
@@ -6,14 +6,31 @@ namespace TaskManagement.Data.Context
 {
     public class TaskContext : IdentityDbContext<User>
     {
-        public TaskContext()
-        {
+        public TaskContext() {}
 
-        }
         public TaskContext(DbContextOptions<TaskContext> options)
         : base(options) { }
 
         public virtual DbSet<Comment> Comments { get; set; }
         public virtual DbSet<Task> Tasks { get; set; }
+        public virtual DbSet<UserTask> UserTasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region Many-To-Many Relationship UserTasks
+            modelBuilder.Entity<UserTask>()
+                .HasKey(userTask => new { userTask.UserId, userTask.TaskId });
+            modelBuilder.Entity<UserTask>()
+                .HasOne(userTask => userTask.Task)
+                .WithMany(user => user.UserTasks)
+                .HasForeignKey(userTask => userTask.TaskId);
+            modelBuilder.Entity<UserTask>()
+             .HasOne(userTask => userTask.User)
+             .WithMany(task => task.UserTasks)
+             .HasForeignKey(userTask => userTask.UserId);
+            #endregion
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
