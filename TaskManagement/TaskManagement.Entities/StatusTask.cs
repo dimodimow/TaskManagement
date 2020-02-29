6560b88cd38d776f6c13790ad0096e911157ee74
Dimarata <ivanov_dimo@abv.bg>
2020-02-29 03:46:57 +0200

    Create StatusTask

---
+++ TaskManagement/TaskManagement.Entities/StatusTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagement.Entities
+{
+  public  class StatusTask 
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
