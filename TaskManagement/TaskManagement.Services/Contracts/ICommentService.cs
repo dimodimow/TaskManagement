48c6049a60afee654c8b39717c78d32d44c1ae1f
Dimarata <ivanov_dimo@abv.bg>
2020-02-29 03:44:52 +0200

    Create interface ICommentService

---
+++ TaskManagement/TaskManagement.Services/Contracts/ICommentService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskManagement.Entities;
+
+namespace TaskManagement.Services.Contracts
+{
+    public interface ICommentService
+    {
+        Task<Comment> CreateComment(string text, string userId, DateTime? reminderDate);
+        Task<List<Comment>> GetAllComments(Guid taskId);
+    }
+}
