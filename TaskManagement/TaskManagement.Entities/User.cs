68d620e273dacd7cd9575e9697111689067b7474
Dimarata <ivanov_dimo@abv.bg>
2020-02-29 03:49:28 +0200

    Modified User class

--- TaskManagement/TaskManagement.Entities/User.cs
+++ TaskManagement/TaskManagement.Entities/User.cs
@@ -6,5 +6,6 @@ namespace TaskManagement.Entities
    public class User : IdentityUser
     {
         public ICollection<UserTask> UserTasks { get; set; }
+        public ICollection<Comment> Comments { get; set; }
     }
 }
