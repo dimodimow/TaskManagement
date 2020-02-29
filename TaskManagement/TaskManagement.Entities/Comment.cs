a3e53896520e4ddc04c6bb178c12fcbb8af4f7b0
Dimarata <ivanov_dimo@abv.bg>
2020-02-29 03:49:03 +0200

    Modified Comment class

--- TaskManagement/TaskManagement.Entities/Comment.cs
+++ TaskManagement/TaskManagement.Entities/Comment.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TaskManagement.Entities
 {
     public class Comment : BaseEntity
     {
-        public string CommentContent { get; set; }
+        [Required]
+        public string Text { get; set; }
         public DateTime? ReminderDate { get; set; }
         public Guid TaskId { get; set; }
         public Task Task { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
     }
 }
