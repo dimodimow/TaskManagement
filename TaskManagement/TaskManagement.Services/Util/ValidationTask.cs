da8ff94e9262293c7b8c837233544db1fcc937c4
Dimarata <ivanov_dimo@abv.bg>
2020-03-02 15:00:21 +0200

    Add Identity

---
+++ TaskManagement/TaskManagement.Services/Util/ValidationTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace TaskManagement.Services.Util
+{
+   public static class ValidationTask
+    {
+        public static void ValidateTaskNameIfIsNull(string name)
+        {
+            if (name == null)
+            {
+                throw new ArgumentException("Task name can not be null");
+            }
+        }
+    }
+}
