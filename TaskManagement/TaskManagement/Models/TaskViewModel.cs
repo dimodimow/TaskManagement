212a3610143425791420b895b2dbbfa924e234a2
Dimarata <ivanov_dimo@abv.bg>
2020-03-02 14:58:24 +0200

    Create TaskViewModel

---
+++ TaskManagement/TaskManagement/Models/TaskViewModel.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using TaskManagement.Entities;
+
+namespace TaskManagement.Web.Models
+{
+    public class TaskViewModel
+    {
+        public Guid Id { get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+        public DateTime DueTime { get; set; }  
+        public List<User> AllUsers { get; set; }
+        public List<User> SelectedUsers { get; set; } 
+        public int TypeTaskId { get; set; }
+        public int StatusTaskId { get; set; }
+        public DateTime? NextActionDate { get; set; }
+    }
+}
