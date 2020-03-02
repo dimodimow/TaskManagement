922a04776bbd0140dfc9b2e93bcc55271a013668
Dimarata <ivanov_dimo@abv.bg>
2020-03-02 14:58:43 +0200

    Create TaskMapper.cs

---
+++ TaskManagement/TaskManagement/Mappers/TaskMapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using TaskManagement.Entities;
+using TaskManagement.Web.Mappers.Contracts;
+using TaskManagement.Web.Models;
+
+namespace TaskManagement.Web.Mappers
+{
+    public class TaskMapper : IMapper<TaskViewModel, Entities.Task>
+    {
+        public TaskViewModel Map(Entities.Task entity)
+        {
+            return new TaskViewModel
+            {
+                TaskName = entity.TaskName,
+                Description = entity.Description,
+                DueTime = entity.DueDate,
+                StatusTaskId = entity.StatusTaskId,
+                TypeTaskId = entity.TypeTaskId,
+                NextActionDate = entity.NextActionDate,
+                AllUsers = new List<User>(),
+                SelectedUsers = new List<User>()
+            };
+        }
+    }
+}
