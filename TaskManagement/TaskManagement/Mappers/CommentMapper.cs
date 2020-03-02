3644f2558e02f4c126318b2f727fe2463bb918a9
Dimarata <ivanov_dimo@abv.bg>
2020-03-02 14:58:51 +0200

    Create CommentMapper.cs

---
+++ TaskManagement/TaskManagement/Mappers/CommentMapper.cs
@@ -0,0 +1,22 @@
+﻿using TaskManagement.Entities;
+using TaskManagement.Web.Mappers.Contracts;
+using TaskManagement.Web.Models;
+
+namespace TaskManagement.Web.Mappers
+{
+    public class CommentMapper : IMapper<CommentViewModel, Comment>
+    {
+        public CommentViewModel Map(Comment entity)
+        {
+            return new CommentViewModel
+            {
+                Id = entity.Id,
+                Text = entity.Text,
+                TypeCommentId = entity.TypeCommentId,
+                ReminderDate = entity.ReminderDate,
+                TaskId = entity.TaskId,
+                UserName = entity.User.UserName
+            };
+        }
+    }
+}
