46366358be87c604c3992e4be8b007915037abaf
Dimarata <ivanov_dimo@abv.bg>
2020-03-03 13:14:47 +0200

    Update ITaskService.cs

--- TaskManagement/TaskManagement.Services/Contracts/ITaskService.cs
+++ TaskManagement/TaskManagement.Services/Contracts/ITaskService.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using TaskManagement.Entities;
+using TaskManagement.Web.Models;
 
 namespace TaskManagement.Services.Contracts
 {
    public interface ITaskService
     {
-        Task<Entities.Task> CreateTask(string taskName, string description, string userId,  DateTime dueDate, int typeTask, ICollection<User> selectedUsers, DateTime? nextActionDate = null);
+        Task<Entities.Task> CreateTask(string taskName, string description, string userId,  DateTime? dueDate, int typeTask, ICollection<UserProxyViewModel> selectedUsers, DateTime? nextActionDate = null);
         Task<List<Entities.Task>> GetAllAsync();
         Task<bool> DeleteTask(Guid taskId);
+        public Task<Entities.Task> FindTaskById(Guid id);
+        Task<Entities.Task> EditTask(Guid taskId, string taskName, string description, DateTime? dueDate, ICollection<UserProxyViewModel> selectedUsers);
     }
 }
