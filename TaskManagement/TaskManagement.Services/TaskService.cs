67c17ef0bc6d34d6e231e3d8acf5b922c6be3d40
Dimarata <ivanov_dimo@abv.bg>
2020-03-03 13:14:37 +0200

    Update TaskService.cs

--- TaskManagement/TaskManagement.Services/TaskService.cs
+++ TaskManagement/TaskManagement.Services/TaskService.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using TaskManagement.Data.Context;
 using TaskManagement.Entities;
 using TaskManagement.Services.Contracts;
+using TaskManagement.Services.Util;
+using TaskManagement.Web.Models;
 
 namespace TaskManagement.Services
 {
@@ -16,9 +18,9 @@ namespace TaskManagement.Services
             this.context = context;
         }
         
-        public async Task<Entities.Task> CreateTask(string taskName,string description,string userId, DateTime dueDate, int typeTask, ICollection<User> selectedUsers, DateTime? nextActionDate = null)
+        public async Task<Entities.Task> CreateTask(string taskName,string description,string userId, DateTime? dueDate, int typeTask, ICollection<UserProxyViewModel> selectedUsers, DateTime? nextActionDate = null)
         {
-
+            ValidationTask.ValidateTaskNameIfIsNull(taskName);
             Guid taskId = Guid.NewGuid();
 
             var task = new Entities.Task
@@ -35,7 +37,7 @@ namespace TaskManagement.Services
             {
                 var userTask = new UserTask
                 {
-                    User = selectedUser,
+                    UserId = selectedUser.UserId,
                     Task = task
                 };
                 await this.context.AddAsync(userTask);
@@ -46,6 +48,40 @@ namespace TaskManagement.Services
             
             return task;
         }
+        public async Task<Entities.Task> EditTask(Guid taskId, string taskName, string description, DateTime? dueDate, ICollection<UserProxyViewModel> selectedUsers)
+        {
+            var task = await this.context.Tasks.SingleOrDefaultAsync(a => a.Id.Equals(taskId));
+
+            ValidationTask.ValidateTaskNameIfIsNull(taskName);
+
+            if (await this.context.Tasks.AnyAsync(n => n.TaskName.Equals(taskName.ToLower()) && n.Id != taskId))
+            {
+                throw new ArgumentException($"Task with Name: {taskName} already exists!");
+            }
+
+            task.TaskName = taskName;
+            task.Description = description;
+            task.DueDate = dueDate;
+
+            //foreach (var selectedUser in selectedUsers)
+            //{
+            //    if (selectedUser.IsChecked)
+            //    {
+            //        var userTask = new UserTask
+            //        {
+            //            UserId = selectedUser.UserId.ToString(),
+            //            Task = task
+            //        };
+            //        await this.context.AddAsync(userTask);
+            //    }
+            //}
+
+            this.context.Tasks.Update(task);
+            await this.context.SaveChangesAsync();
+
+            return task;
+        }
+
 
         public async Task<List<Entities.Task>> GetAllAsync()
         {
@@ -66,6 +102,13 @@ namespace TaskManagement.Services
             this.context.Tasks.Remove(task);
             await this.context.SaveChangesAsync();
             return true;
+        }
+
+        public async Task<Entities.Task> FindTaskById(Guid id)
+        {
+            var task = await this.context.Tasks.Include(x => x.Comments).FirstOrDefaultAsync(x => x.Id == id);
+
+            return task;
         }
     }
 }
